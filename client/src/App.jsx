a097a4ef78790af776681a79860e0869291956e5
Aditya-Padwal <aditya.padwal3102@gmail.com>
2024-01-03 13:38:03 +0530

    basic project setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar';
+import axios from 'axios';
+import Layout from './components/Layout';
+import IndexPage from './components/IndexPage'
+import LoginPage from './components/LoginPage'
+import SignupPage from './components/SignupPage'
+import TempPage from './components/TempPage';
+import { Route, Routes } from 'react-router-dom';
+
+// configuring axios
+axios.defaults.baseURL = `http://localhost:8000`;
+axios.defaults.withCredentials = true;
+
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Layout />} >
+
+          <Route index element={< IndexPage/>}/>
+          <Route path='/login' element={< LoginPage/>}/>
+          <Route path='/signup' element={< SignupPage/>}/>
+          <Route path='/temp' element={< TempPage/>}/> {/* Only for testing */}
+          
+        </Route>
+      </Routes>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,42 @@
 // Importing all the modules
 const express = require('express'); // express framework
+const cors = require('cors'); // enabling CORS
+const cookieParser = require('cookie-parser'); // parsing cookies
+const mongoose = require("mongoose"); // ODM
+
+// Importing routes
+const tempRoutes = require('./routes/temp');
 
 // Creating an instance of the express application
 const app = express();
 
-app.listen(8000, () => {
-    console.log(`Server is running on port 8000`);
-});
+// Middlewares
+app.use(express.json()); // parsing incoming json data from requests 
+app.use(cookieParser()); // parsing cookies
+
+// Implementing cors
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
+
+// Testing request
+// app.get('/test', (req, res) => {
+//     res.json('Test ok!');
+// });
+
+// Implementing the above imported routes
+app.use(tempRoutes);
+
+// Running the express application
+mongoose
+    .connect('mongodb+srv://aditya:FaN34I0sUEp6Wtl9@cluster0.ksz4zpz.mongodb.net/?retryWrites=true&w=majority')
+    .then((result) => {
+        app.listen(8000);
+    })
+    .then(() => {
+        console.log("Server running on port 8000");
+    })
+    .catch((err) => {
+        console.log(err);
+    });

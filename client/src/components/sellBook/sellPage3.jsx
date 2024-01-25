e76f54ac7776a4b549b245ad211d62812e400204
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2024-01-25 23:45:37 +0530

    added separate component for each page

--- client/src/components/sellBook/sellPage3.jsx
+++ client/src/components/sellBook/sellPage3.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { Box, Grid, TextField } from "@mui/material";
+import React from "react";
 
-export default function sellPage3() {
+export default function SellPage3({
+  activeStep,
+  userName,
+  mobileNo,
+  city,
+  handleUserNameChange,
+  handleMobileNoChange,
+  handleCityChange,
+}) {
   return (
-    <div>sellPage3</div>
-  )
+    <div>
+      {activeStep === 2 && (
+        <Box>
+          {/* Form 3 Fields */}
+          <TextField
+            sx={{ width: "100%" }}
+            label="Your Name"
+            margin="normal"
+            required
+            variant="outlined"
+            value={userName}
+            onChange={handleUserNameChange}
+          />
+          <Grid container spacing={{ xs: 2, md: 10 }}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                sx={{ width: "100%" }}
+                label="Mobile No."
+                type="number"
+                margin="normal"
+                variant="outlined"
+                required
+                value={mobileNo}
+                onChange={handleMobileNoChange}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <TextField
+                sx={{ width: "100%" }}
+                label="City"
+                type="text"
+                margin="normal"
+                variant="outlined"
+                required
+                value={city}
+                onChange={handleCityChange}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      )}
+    </div>
+  );
 }
